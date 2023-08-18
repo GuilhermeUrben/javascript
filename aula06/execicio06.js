65a08fde57221d63f6f1cef4d47b0d94651f08ec
Guilherme Urben <126745231+GuilhermeUrben@users.noreply.github.com>
2023-08-18 07:16:52 -0300

    Update execicio06.js

--- aula06/execicio06.js
+++ aula06/execicio06.js
@@ -174,3 +174,63 @@ if (valor !== undefined) {
     console.log("\nVocê escolheu: " + bebida + ".");
     console.log("Valor a pagar: R$" + valor.toFixed(2));
 }
+
+const prompt = require("prompt-sync")();
+
+// Solicitar a escolha da bebida ao cliente
+var bebida = prompt("Escolha uma bebida: cafe, leite ou cha: ");
+
+// Inicializar a variável para armazenar o valor da bebida
+var valor;
+
+// Avaliar a opção selecionada
+switch (bebida.toLowerCase()) {
+    case "cafe":
+        valor = 2.50;
+        break;
+    case "leite":
+        valor = 1.50;
+        break;
+    case "cha":
+        valor = 1.00;
+        break;
+    default:
+        // Se a opção for inválida.
+        console.log("Escolha inválida. Por favor, escolha entre cafe, leite ou cha.");
+}
+
+// Exibir mensagem personalizada com o nome da bebida e valor formatado
+if (valor !== undefined) {
+    console.log("\nVocê escolheu: " + bebida + ".");
+    console.log("Valor a pagar: R$" + valor.toFixed(2));
+}
+const prompt = require("prompt-sync")();
+
+// Solicitar a escolha da bebida ao cliente
+var bebida = prompt("Escolha uma bebida: cafe, leite ou cha: ");
+
+// Inicializar a variável para armazenar o valor da bebida
+var valor;
+
+// Avaliar a opção selecionada
+switch (bebida.toLowerCase()) {
+    case "cafe":
+        valor = 2.50;
+        break;
+    case "leite":
+        valor = 1.50;
+        break;
+    case "cha":
+        valor = 1.00;
+        break;
+    default:
+        // Se a opção for inválida.
+        console.log("Escolha inválida. Por favor, escolha entre cafe, leite ou cha.");
+}
+
+// Exibir mensagem personalizada com o nome da bebida e valor formatado
+if (valor !== undefined) {
+    console.log("\nVocê escolheu: " + bebida + ".");
+    console.log("Valor a pagar: R$" + valor.toFixed(2));
+}
+
