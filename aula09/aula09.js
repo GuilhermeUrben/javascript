cfaa8f2d9c9322a1dac23eeb06e26317186125a4
Guilherme Urben <126745231+GuilhermeUrben@users.noreply.github.com>
2023-08-28 21:48:55 -0300

    att

--- aula09/aula09.js
+++ aula09/aula09.js
@@ -1,33 +1,21 @@
-function criaarFuncionario(nome, sobrenome, a, p) {
-    return {
-        nome,
-        sobrenome,
-        altura: a,
-        peso: p,
-        calculoImc() {
-            const indice = this.peso / (this.altura ** 2).toFixed(1)
-            if (indice < 18.5) {
-                console.log("Você está abaixo do peso.")
+class Pessoa{
 
-            } else if (indice >= 18.5 && indice <= 24.9) {
-                console.log("Você está com peso normal.")
-
-            } else if (indice >= 25 && indice <= 29.9) {
-                console.log("Você está acima do peso.")
-
-            } else if (indice >= 30 && indice <= 34.9) {
-                console.log("Você está com obesidade 1.")
-
-            } else if (indice >= 35 && indice <= 39.9) {
-                console.log("Você está com obesidade 2.")
-
-            } else if (indice > 40) {
-                console.log("Você está com obesidade mórbida.")
-
-            }
-        }
+    constructor(nome, sobrenome){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
     }
+
+    falar(){
+        console.log("Olá " + this.nome);
+    }
+
+    get nomeCompleto(){
+        console.log("Olá " + this.nome + " " + this.sobrenome);
+    }
+
 }
 
-const p1 = criaarFuncionario("Guilherme", "Urben", 1.75, 84);
-p1.calculoImc();
+
+p1 = new Pessoa("Fábio", "Silva");
+p1.falar();
+p1.nomeCompleto
