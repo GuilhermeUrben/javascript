03365a2849b414c101812394eebf0d1d914d7aad
Guilherme Urben <126745231+GuilhermeUrben@users.noreply.github.com>
2023-08-30 22:34:00 -0300

    Update arquivoclasse01.js

--- aula09/arquivoclasse01.js
+++ aula09/arquivoclasse01.js
@@ -1,11 +1,11 @@
-class DispositivoEletronico{
-    constructor(nome){
+class DispositivoEletronico {
+    constructor(nome) {
         this.nome = nome;
         this.ligado = false;
     }
 
-    ligar(){
-        if(this.ligado){
+    ligar() {
+        if (this.ligado) {
             console.log("Já está ligado.")
             return;
         }
@@ -15,14 +15,14 @@ class DispositivoEletronico{
 
 }
 
-class SmartPhone extends DispositivoEletronico{
-    constructor(nome,cor,modelo){
+class SmartPhone extends DispositivoEletronico {
+    constructor(nome, cor, modelo) {
         super(nome);
         this.cor = cor;
         this.modelo = modelo;
 
 
-    }    
+    }
 
 }
 
@@ -33,14 +33,14 @@ s1.ligar();
 
 
 
-class DispositivoEletronico{
-    constructor(nome){
+class DispositivoEletronico {
+    constructor(nome) {
         this.nome = nome;
         this.ligado = false;
     }
 
-    ligar(){
-        if(this.ligado){
+    ligar() {
+        if (this.ligado) {
             console.log("Já está ligado.")
             return;
         }
@@ -50,14 +50,84 @@ class DispositivoEletronico{
 
 }
 
-class SmartPhone extends DispositivoEletronico{
-    constructor(nome,cor,modelo){
+class SmartPhone extends DispositivoEletronico {
+    constructor(nome, cor, modelo) {
         super(nome);
         this.cor = cor;
         this.modelo = modelo;
 
 
-    }    
+    }
+
+}
+
+var s1 = new SmartPhone("Samsung", "Preto", "S20");
+s1.ligar();
+console.log(s1);
+s1.ligar();
+
+
+
+class DispositivoEletronico {
+    constructor(nome) {
+        this.nome = nome;
+        this.ligado = false;
+    }
+
+    ligar() {
+        if (this.ligado) {
+            console.log("Já está ligado.")
+            return;
+        }
+        this.ligado = true;
+    }
+
+
+}
+
+class SmartPhone extends DispositivoEletronico {
+    constructor(nome, cor, modelo) {
+        super(nome);
+        this.cor = cor;
+        this.modelo = modelo;
+
+
+    }
+
+}
+
+var s1 = new SmartPhone("Samsung", "Preto", "S20");
+s1.ligar();
+console.log(s1);
+s1.ligar();
+
+
+
+class DispositivoEletronico {
+    constructor(nome) {
+        this.nome = nome;
+        this.ligado = false;
+    }
+
+    ligar() {
+        if (this.ligado) {
+            console.log("Já está ligado.")
+            return;
+        }
+        this.ligado = true;
+    }
+
+
+}
+
+class SmartPhone extends DispositivoEletronico {
+    constructor(nome, cor, modelo) {
+        super(nome);
+        this.cor = cor;
+        this.modelo = modelo;
+
+
+    }
 
 }
 
