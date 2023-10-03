88818e78d8eecfbeae93878390ead5814e65d471
GuilhermeUrben <guilhermeurben@gmail.com>
2023-10-02 22:47:32 -0300

    Update aula13.js

--- descomplica/aula13/aula13.js
+++ descomplica/aula13/aula13.js
@@ -12,3 +12,17 @@ const pessoaObj = JSON.parse(pessoajson)
 console.log(pessoaObj)
 
 
+const pessoa = {
+    nome: 'João', 
+    idade: 25,
+    profissão: "Deseolvovedor" 
+}
+
+const pessoajson = JSON.stringify(pessoa)
+console.log(pessoa)
+console.log(pessoajson)
+
+const pessoaObj = JSON.parse(pessoajson)
+console.log(pessoaObj)
+
+
